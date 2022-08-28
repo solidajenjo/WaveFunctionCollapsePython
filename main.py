9bb4e58bbb92a50dbc4ec6ec4ff8db694c607fb7
solidajenjo <rubenajenjo@mpg.io>
2022-08-28 11:33:36 +0200

    Source image slicing | Generated 4 versions per tile (flipping) | Tile array inspector

--- main.py
+++ main.py
@@ -1,44 +1,64 @@
 import pygame
 import math
 
-SOURCE_SIZE = 32
+SOURCE_SIZE = 256
+
+def slice_tiles(tileSize, divisions, logoStr):
+    tilesArray = []
+    lineSizeInBytes = tileSize * 3
+    for i in range (divisions): #vert
+        for j in range (divisions): #horizontal            
+            
+            newTile = []            
+            newTileHorizontalFlipped = []
+
+            for y in range (tileSize * i, tileSize * i + tileSize):
+                vertOffset = y * SOURCE_SIZE * 3 + tileSize * j * 3
+                xRange = vertOffset + lineSizeInBytes
+                newTileLine = logoStr[slice(vertOffset, xRange, 1)]
+                newTile += newTileLine
+                newTileHorizontalFlipped += newTileLine[::-1]
+
+            tilesArray += [newTile]
+            tilesArray += [newTile[::-1]] #flip vertical
+            tilesArray += [newTileHorizontalFlipped]
+            tilesArray += [newTileHorizontalFlipped[::-1]] #flip vertical
+
+    tiles = []
+    for t in (tilesArray):
+        tiles += [pygame.image.frombuffer(bytes(t), (tileSize, tileSize), "RGB")]
+    
+    return tiles
 
 def main():
 
     pygame.init()
 
-    sourceImage = pygame.image.load("logo32x32.png")
+    sourceImage = pygame.image.load("logo.png")
     pygame.display.set_icon(sourceImage)
     pygame.display.set_caption("Wave Function Collapse")
      
     screen = pygame.display.set_mode((800,800))
      
     running = True
-    
+    divisions = 4
     logoStr = pygame.image.tostring(sourceImage, "RGB")
-    tileSize = int(SOURCE_SIZE / 2)
+    tileSize = int(SOURCE_SIZE / divisions)
 
-    newTile = []
-    lineSizeInBytes = tileSize * 3
-    for y in range (0, tileSize):
-        vertOffset = y * SOURCE_SIZE * 3
-        xRange = vertOffset + lineSizeInBytes
-        print(vertOffset, xRange)
-        newTileLine = logoStr[slice(vertOffset, xRange, 1)]
-        newTile += newTileLine
-
-    newTile = bytes(newTile)
-
-    tile = pygame.image.frombuffer(newTile, (tileSize, tileSize), "RGB")
-
-    print (len(logoStr))
+    tiles = slice_tiles(tileSize, divisions, logoStr)
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.blit(sourceImage, (100,100))
-        screen.blit(tile, (150,100))
+        lineJump = 0
+        column = 0
+        for i in range (len(tiles)):
+            if i % 8 == 0:
+                lineJump += 1
+                column = 0
+            screen.blit(tiles[i], ((tileSize + 5) * column, 100 + (tileSize +5) * lineJump))
+            column += 1
 
         pygame.display.flip()
     
